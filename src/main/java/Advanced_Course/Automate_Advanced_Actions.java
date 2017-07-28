30db95ccbf3386bc2e25200f9b863ba754cdf6d7
Pranjali Jaiswal <pranjalijaiswal@qainfotech.com>
2017-07-28 09:13:21 +0530

    =Update For database in advanced Course

--- src/main/java/Advanced_Course/Automate_Advanced_Actions.java
+++ src/main/java/Advanced_Course/Automate_Advanced_Actions.java
@@ -1,15 +1,12 @@
 package Advanced_Course;
 
-import java.lang.reflect.Array;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,20 +20,16 @@ public class Automate_Advanced_Actions {
 	String dbUrl="jdbc:mysql://10.0.1.86/tatoc";
 	String username="tatocuser";
 	String password="tatoc01";
-	String name="";
-	String credential="";
-	String identity="";
 	ResultSet result;
-	String credential_name[]=new String[26];
-	String credential_pwd[]=new String[26];
-	String symbols[]=new String[30];
 	private By advance=By.linkText("Advanced Course");
 	private By menu_2=By.cssSelector(".m2");
-	private By go_next=By.xpath("//span[contains(@class,'menuitem')]  [contains(text(),'Go Next')]");
+	private By go_next=By.xpath("//div[contains(@class,'menutop m2')] /span[contains(text(),'Go Next')]");
 	private By symbol=By.id("symboldisplay");
 	private By user_name=By.id("name");
 	private By passkey=By.id("passkey");
 	private By proceed= By.id("submit");
+	String name="";
+	String key="";
 	
 	public Automate_Advanced_Actions(WebDriver driver) {
 		Automate_Advanced_Actions.driver=driver;
@@ -46,7 +39,7 @@ public class Automate_Advanced_Actions {
 	{
 		driver.findElement(advance).click();
 		hover_menu();
-		get_values_from_database();
+		get_Credentials();
 		enter_Credentials();
 	}
 	public void getURL(String URL)
@@ -70,52 +63,26 @@ public class Automate_Advanced_Actions {
 		Statement statement = con.createStatement();	
 		result= statement.executeQuery(Query);	
 	}
-	public void Credential_database() throws SQLException
+	
+	public int get_Id() throws ClassNotFoundException, SQLException
 	{
-		int index=0;
-		while (result.next()){
-	    	 name = result.getString(2);
-	    	 credential = result.getString(3);
-	    	 credential_name[index]=name;
-	    	 credential_pwd[index]=credential;
-	    	 index++;
-			}
-	}
-	public void Identity_database() throws SQLException
-	{
-		int index=0;
-		while (result.next()){
-	    	 identity = result.getString(2);
-	         symbols[index]=identity;
-	         index++;
-			}		
-	}
-	public void get_values_from_database() throws ClassNotFoundException, SQLException
-	{
-		connection("Select * from credentials");
-		Credential_database();
-		connection("Select * from identity");
-		Identity_database();
-	}
-	public int get_Credentials()
-	{
-		int tag=0;
 		String text=driver.findElement(symbol).getText();
-		for(int index=0;index<symbols.length;index++)
-		{
-			if(text.equalsIgnoreCase(symbols[index]))
-			{
-				tag=index;
-				break;
-			}
-		}
-		return tag;
+		connection("select id from identity where symbol='" + text+"'");
+		result.next();
+		return result.getInt(1);
+	}
+	
+	public void get_Credentials() throws ClassNotFoundException, SQLException
+	{
+		connection("select name,passkey from credentials where id='"+get_Id()+"'");
+		result.next();
+		name=result.getString(1);
+		key=result.getString(2);
 	}
 	public void enter_Credentials()
 	{
-		int value=get_Credentials();
-		driver.findElement(user_name).sendKeys(""+credential_name[value]+"");
-		driver.findElement(passkey).sendKeys(""+credential_pwd[value]+"");
+		driver.findElement(user_name).sendKeys(""+name+"");
+		driver.findElement(passkey).sendKeys(""+key+"");
 		driver.findElement(proceed).click();
 	}
 
